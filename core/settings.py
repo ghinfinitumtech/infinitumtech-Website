eb87e41ce04679609a3354f3f11c19c0e1ca0b54
LE0-MAhendra <mahender.kurikyala11@gmail.com>
2024-03-18 11:45:01 +0530

    v1 finished

---
+++ core/settings.py
@@ -0,0 +1,146 @@
+
+from pathlib import Path
+import os
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-c31$(8faq6^!3_=f-3&@$b545ww*3$o1q9g5b-3-ce&f9flb+r'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myApp',
+'phonenumber_field',
+
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'myApp.context_processors.company_details',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+
+# settings.py
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # or 'django.db.backends.mysql' for MariaDB
+#         'NAME': 'tech_infinitum',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',  # or '127.0.0.1'
+#         'PORT': '3306',  # MySQL default port
+#     }
+# }
+
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+#xampp database configarations
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#custom static fils
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# media fils 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ myApp/models.py
@@ -0,0 +1,121 @@
+from typing import Any
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+# Create your models here.
+
+
+class Banner(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images")
+    
+    def __str__(self):
+        return self.name
+
+class Technologie(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="technology",null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="projects",null=True,blank=True)
+    link = models.CharField(max_length=100, null=True, blank=True)
+    git_hub = models.CharField(max_length=100, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=100)
+    banner_image = models.ImageField(upload_to="services/banner",default="img/banner/page-title.jpg")
+    image = models.ImageField(upload_to="services/mages")
+    icons=models.ImageField(upload_to='services/mages/logos',null=True,blank=True)
+    content = models.CharField(max_length=80)
+    descriptions = models.TextField(max_length=1000)
+    technology = models.ForeignKey(Technologie, on_delete=models.CASCADE, null=True, blank=True)
+    projects = models.ManyToManyField(Project, blank=True, null=True)
+    main_services= models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Testimonials(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='Testimonials/')
+    designation=models.CharField(max_length=100)
+    feedback=models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class TeamMembers(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='team/')
+    designation=models.CharField(max_length=100)
+    facebook=models.CharField(max_length=100,null=True,blank=True)
+    twitter=models.CharField(max_length=100,null=True,blank=True)
+    youtube=models.CharField(max_length=100,null=True,blank=True)
+    whatsapp=models.CharField(max_length=100,null=True,blank=True)
+    linkedin=models.CharField(max_length=100,null=True,blank=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
+class CareerApplication(models.Model):
+
+    GENDER_CHOICES = [
+
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ]
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    resume = models.FileField(upload_to='resumes/')
+    phone = PhoneNumberField(null=True, blank=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    date = models.DateField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject  = models.CharField(max_length=100)
+    phone =PhoneNumberField(null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class CompanyDetails(models.Model):
+    name=models.CharField(max_length=100)
+    logo=models.ImageField(upload_to='Company/')
+    email=models.EmailField()
+    address=models.CharField(max_length=200)
+    phone=PhoneNumberField(null=False, blank=False)
+    facebook=models.CharField(max_length=100,null=True,blank=True)
+    twitter=models.CharField(max_length=100,null=True,blank=True)
+    youtube=models.CharField(max_length=100,null=True,blank=True)
+    whatsapp=models.CharField(max_length=100,null=True,blank=True)
+    linkedin=models.CharField(max_length=100,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
