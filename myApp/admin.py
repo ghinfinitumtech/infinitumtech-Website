f4d80d4bc91ec926ba8c53f77ed5e56386eea188
Mahender-InfinitumTech <infinitumtech-d01@outlook.com>
2024-11-01 14:20:08 +0530

    admin only one company register

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from myApp.models import *
+
 # Register your models here.
 
 admin.site.register(Banner)
@@ -10,4 +11,14 @@ admin.site.register(Testimonials)
 admin.site.register(TeamMembers)
 admin.site.register(CareerApplication)
 admin.site.register(Contact)
-admin.site.register(CompanyDetails)
+
+
+@admin.register(CompanyDetails)
+class CompanyDetailsAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        # Allow adding only if there are no instances of CompanyDetails
+        return not CompanyDetails.objects.exists()
+
+    def has_delete_permission(self, request, obj=None):
+        # Prevent deletion of CompanyDetails instance
+        return False
