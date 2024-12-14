e7949e8c0cfe542217b2f52fbab6f9b23da9bd8c
Mahender-InfinitumTech <infinitumtech-d01@outlook.com>
2024-12-14 17:48:32 +0530

    update on project links

--- myApp/models.py
+++ myApp/models.py
@@ -26,6 +26,11 @@ class Project(models.Model):
     image = models.ImageField(upload_to="projects", null=True, blank=True)
     link = models.CharField(max_length=100, null=True, blank=True)
     git_hub = models.CharField(max_length=100, null=True, blank=True)
+    facebook = models.CharField(max_length=100, null=True, blank=True)
+    twitter = models.CharField(max_length=100, null=True, blank=True)
+    youtube = models.CharField(max_length=100, null=True, blank=True)
+    whatsapp = models.CharField(max_length=100, null=True, blank=True)
+    linkedin = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
